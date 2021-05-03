572c43f3f0702e0ab8818c11e852d22326d17a8b
Víctor Andrés Rojas <victandres98@gmail.com>
2021-05-02 23:29:04 -0500

    Custom Design: Background Gradient Color and StatusBar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 import 'package:flutter_designs/src/pages/basic_page.dart';
+import 'package:flutter_designs/src/pages/custom_page.dart';
 import 'package:flutter_designs/src/pages/scroll_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,13 +10,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(
+    //   SystemUiOverlayStyle.light.copyWith(
+    //     statusBarColor: Colors.pinkAccent,
+    //   ),
+    // );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: "scroll",
+      initialRoute: "custom",
       routes: {
         "basic": (BuildContext context) => BasicPage(),
         "scroll": (BuildContext context) => ScrollPage(),
+        "custom": (BuildContext context) => CustomPage(),
       },
     );
   }
---
+++ lib/src/pages/custom_page.dart
@@ -0,0 +1,98 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CustomPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _backgroundApp(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                _titles(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _backgroundApp() {
+    final gradient = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0),
+          ],
+        ),
+      ),
+    );
+
+    final pinkBox = Transform.rotate(
+      angle: -pi / 5.0,
+      child: Container(
+        height: 360.0,
+        width: 360.0,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(236, 98, 188, 1.0),
+                Color.fromRGBO(241, 142, 172, 1.0),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(90.0)),
+      ),
+    );
+
+    return Stack(
+      children: [
+        gradient,
+        Positioned(
+          top: -100.0,
+          left: -50.0,
+          child: pinkBox,
+        ),
+      ],
+    );
+  }
+
+  Widget _titles() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Classify Transaction",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Classify this transaction into a particular category.",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
